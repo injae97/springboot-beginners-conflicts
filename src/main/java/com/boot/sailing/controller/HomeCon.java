cd5a5ba7f2aa88ca8ca65aebd2d1175fecf607dd
InJeKim97 <injaekim97@naver.com>
2023-02-04 20:15:55 +0900

    website in development v0.0

--- src/main/java/com/boot/sailing/controller/HomeCon.java
+++ src/main/java/com/boot/sailing/controller/HomeCon.java
@@ -2,6 +2,7 @@ package com.boot.sailing.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeCon {
@@ -10,5 +11,12 @@ public class HomeCon {
 	@GetMapping("/home")
 	public String doHome() {
 		return "/home/home"; // template > home > home.html 화면을 보여줌
+	}
+	
+	@GetMapping("/rest2")
+	@ResponseBody
+	public String doRest() {
+		String strHtml = "Hi @RestController -> @ResponseBody + @Controller";        
+		return strHtml;
 	}
 }
