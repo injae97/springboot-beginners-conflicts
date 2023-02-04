88a305b13af2ef91727110a439af818f667aedf3
InJeKim97 <injaekim97@naver.com>
2023-02-04 17:29:54 +0900

    website in development v0.0

--- src/main/java/com/boot/sailing/controller/MenuCon.java
+++ src/main/java/com/boot/sailing/controller/MenuCon.java
@@ -1,13 +1,47 @@
 package com.boot.sailing.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MenuCon {
 	
 	@GetMapping("/menu")
-	public String doHome() {
+	public String doMenu(Model model) {
+		
+		// Data Create - List, Map
+		List<Map<String, Object>> list = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+
+	    map.put("No", "1");
+	    map.put("name", "아이스아메");
+	    map.put("kind", "커피");
+	    map.put("price", "5,000");
+	    map.put("reg_day", "2020.10.29");
+	    map.put("mod_day", "2021.10.29");
+	    list.add(map);
+
+	    map.clear();
+
+	    map.put("No", "2");
+	    map.put("name", "카푸치노");
+	    map.put("kind", "커피");
+	    map.put("price", "6,000");
+	    map.put("reg_day", "2020.10.29");
+	    map.put("mod_day", "2021.10.29");
+	    list.add(map);
+	    
+	    
+		// Data 전송 - Model
+	    model.addAttribute("list", list);
+	    model.addAttribute("hello", "========== MenuCon ==========");
+	    
 		return "/menu/menu"; 
-	}
+	}    
 }
--- src/main/java/com/boot/sailing/controller/OrderCon.java
+++ src/main/java/com/boot/sailing/controller/OrderCon.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class OrderCon {
 	
 	@GetMapping("/order")
-	public String doHome() {
+	public String doOrder() {
 		return "/order/order"; 
 	}
 }
